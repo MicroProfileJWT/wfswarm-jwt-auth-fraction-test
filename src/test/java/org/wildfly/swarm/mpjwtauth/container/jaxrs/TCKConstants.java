5613c07556b02696b52c390ae042250d4cfd0afd
Scott Stark <starksm64@gmail.com>
2017-08-28 16:01:33 -0700

    Add ClaimValue injection test
    Signed-off-by: Scott Stark <starksm64@gmail.com>

---
+++ src/test/java/org/wildfly/swarm/mpjwtauth/container/jaxrs/TCKConstants.java
@@ -0,0 +1,16 @@
+package org.wildfly.swarm.mpjwtauth.container.jaxrs;
+
+public class TCKConstants {
+    // TestNG groups
+    public static final String TEST_GROUP_UTILS="utils";
+    public static final String TEST_GROUP_JWT="jwt";
+    public static final String TEST_GROUP_JAXRS="jaxrs";
+    public static final String TEST_GROUP_CDI="cdi";
+    public static final String TEST_GROUP_CDI_JSON="cdi-json";
+    public static final String TEST_GROUP_EJB="ejb-optional";
+    public static final String TEST_GROUP_SERVLET="servlet-optional";
+    public static final String TEST_GROUP_JACC="jacc-optional";
+    public static final String TEST_GROUP_DEBUG="debug";
+
+    private TCKConstants() {}
+}
