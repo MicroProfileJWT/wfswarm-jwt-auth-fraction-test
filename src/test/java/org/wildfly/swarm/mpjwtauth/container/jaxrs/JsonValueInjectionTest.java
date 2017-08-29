b22fe983906f827253fd82f61adb96996204b63b
Scott Stark <starksm64@gmail.com>
2017-08-28 18:30:47 -0700

    sync with TCK changes and JsonValue provider
    Signed-off-by: Scott Stark <starksm64@gmail.com>

---
+++ src/test/java/org/wildfly/swarm/mpjwtauth/container/jaxrs/JsonValueInjectionTest.java
@@ -0,0 +1,268 @@
+package org.wildfly.swarm.mpjwtauth.container.jaxrs;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+
+import javax.enterprise.inject.spi.Extension;
+import javax.json.JsonObject;
+import javax.security.enterprise.CallerPrincipal;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import io.undertow.servlet.ServletExtension;
+import org.eclipse.microprofile.jwt.Claims;
+import org.eclipse.microprofile.jwt.JsonWebToken;
+import org.eclipse.microprofile.jwt.impl.DefaultJWTCallerPrincipalFactory;
+import org.eclipse.microprofile.jwt.principal.JWTCallerPrincipal;
+import org.eclipse.microprofile.jwt.principal.JWTCallerPrincipalFactory;
+import org.eclipse.microprofile.jwt.tck.util.TokenUtils;
+import org.eclipse.microprofile.jwt.wfswarm.JWTAuthMethodExtension;
+import org.eclipse.microprofile.jwt.wfswarm.cdi.MPJWTExtension;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.arquillian.testng.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.resolver.api.maven.ConfigurableMavenResolverSystem;
+import org.jboss.shrinkwrap.resolver.api.maven.Maven;
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static org.wildfly.swarm.mpjwtauth.container.jaxrs.TCKConstants.TEST_GROUP_CDI_JSON;
+
+/**
+ * Tests of injection JsonWebToken claims using the {@linkplain javax.inject.Provider} interface.
+ */
+public class JsonValueInjectionTest extends Arquillian {
+
+    /**
+     * The test generated JWT token string
+     */
+    private static String token;
+    // Time claims in the token
+    private static Long iatClaim;
+    private static Long authTimeClaim;
+    private static Long expClaim;
+
+    /**
+     * The base URL for the container under test
+     */
+    @ArquillianResource
+    private URL baseURL;
+
+    /**
+     * Create a CDI aware base web application archive
+     * @return the base base web application archive
+     * @throws IOException - on resource failure
+     */
+    @Deployment(testable=true)
+    public static WebArchive createDeployment() throws IOException {
+        //System.setProperty("swarm.resolver.offline", "true");
+        //System.setProperty("swarm.debug.port", "8888");
+        //System.setProperty("org.jboss.weld.development", "true");
+        //System.setProperty("org.jboss.weld.probe.exportDataAfterDeployment", "/tmp/cdi.out");
+        ConfigurableMavenResolverSystem resolver = Maven.configureResolver().workOffline();
+        File wfswarmauth = resolver.resolve("org.eclipse.microprofile.jwt:jwt-auth-wfswarm:1.0-SNAPSHOT").withoutTransitivity().asSingleFile();
+        File[] resteasy = resolver.resolve("org.jboss.resteasy:resteasy-json-p-provider:3.0.6.Final").withTransitivity().asFile();
+        File[] ri = resolver.resolve("org.eclipse.microprofile.jwt:jwt-auth-principal-prototype:1.0-SNAPSHOT").withTransitivity().asFile();
+        URL publicKey = JsonValueInjectionTest.class.getResource("/publicKey.pem");
+        WebArchive webArchive = ShrinkWrap
+                .create(WebArchive.class, "JsonValueInjectionTest.war")
+                .addAsLibraries(wfswarmauth)
+                .addAsLibraries(ri)
+                .addAsLibraries(resteasy)
+                .addAsResource(publicKey, "/publicKey.pem")
+                .addAsManifestResource(publicKey, "/MP-JWT-SIGNER")
+                .addAsResource("project-defaults.yml", "/project-defaults.yml")
+                //.addAsResource("project-defaults-basic.yml", "/project-defaults.yml")
+                .addPackage(JWTCallerPrincipal.class.getPackage())
+                .addClass(JsonValuejectionEndpoint.class)
+                .addClass(JsonWebToken.class)
+                .addClass(CallerPrincipal.class)
+
+                .addAsServiceProvider(JWTCallerPrincipalFactory.class, DefaultJWTCallerPrincipalFactory.class)
+                .addAsServiceProvider(ServletExtension.class, JWTAuthMethodExtension.class)
+                .addAsServiceProvider(Extension.class, MPJWTExtension.class)
+                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
+                .addAsWebInfResource("jwt-roles.properties", "classes/jwt-roles.properties")
+                .addAsWebInfResource("WEB-INF/web.xml", "web.xml")
+                .addAsWebInfResource("WEB-INF/jboss-web.xml", "jboss-web.xml")
+                ;
+        System.out.printf("WebArchive: %s\n", webArchive.toString(true));
+        return webArchive;
+    }
+
+    @BeforeClass(alwaysRun=true)
+    public static void generateToken() throws Exception {
+        HashMap<String, Long> timeClaims = new HashMap<>();
+        token = TokenUtils.generateTokenString("/RolesEndpoint.json", null, timeClaims);
+        iatClaim = timeClaims.get(Claims.iat.name());
+        authTimeClaim = timeClaims.get(Claims.auth_time.name());
+        expClaim = timeClaims.get(Claims.exp.name());
+    }
+
+    @RunAsClient
+    @Test(groups = TEST_GROUP_CDI_JSON,
+            description = "Verify that the injected token issuer claim is as expected")
+    public void verifyIssuerClaim() throws Exception {
+        Reporter.log("Begin verifyIssuerClaim");
+        String uri = baseURL.toExternalForm() + "/endp/verifyInjectedIssuer";
+        WebTarget echoEndpointTarget = ClientBuilder.newClient()
+                .target(uri)
+                .queryParam(Claims.iss.name(), "https://server.example.com")
+                .queryParam(Claims.auth_time.name(), authTimeClaim);
+        Response response = echoEndpointTarget.request(MediaType.APPLICATION_JSON).header(HttpHeaders.AUTHORIZATION, "Bearer " + token).get();
+        Assert.assertEquals(response.getStatus(), HttpURLConnection.HTTP_OK);
+        JsonObject reply = response.readEntity(JsonObject.class);
+        System.out.println(reply);
+        Reporter.log(reply.toString());
+        Assert.assertTrue(reply.getBoolean("pass"), reply.getString("msg"));
+    }
+    @RunAsClient
+    @Test(groups = TEST_GROUP_CDI_JSON,
+            description = "Verify that the injected raw token claim is as expected")
+    public void verifyInjectedRawToken() throws Exception {
+        Reporter.log("Begin verifyInjectedRawToken\n");
+        String uri = baseURL.toExternalForm() + "/endp/verifyInjectedRawToken";
+        WebTarget echoEndpointTarget = ClientBuilder.newClient()
+                .target(uri)
+                .queryParam(Claims.raw_token.name(), token)
+                .queryParam(Claims.auth_time.name(), authTimeClaim);
+        Response response = echoEndpointTarget.request(MediaType.APPLICATION_JSON).header(HttpHeaders.AUTHORIZATION, "Bearer " + token).get();
+        Assert.assertEquals(response.getStatus(), HttpURLConnection.HTTP_OK);
+        JsonObject reply = response.readEntity(JsonObject.class);
+        System.out.println(reply);
+        Reporter.log(reply.toString());
+        Assert.assertTrue(reply.getBoolean("pass"), reply.getString("msg"));
+    }
+    @RunAsClient
+    @Test(groups = TEST_GROUP_CDI_JSON,
+            description = "Verify that the injected jti claim is as expected")
+    public void verifyInjectedJTI() throws Exception {
+        Reporter.log("Begin verifyInjectedJTI\n");
+        String uri = baseURL.toExternalForm() + "/endp/verifyInjectedJTI";
+        WebTarget echoEndpointTarget = ClientBuilder.newClient()
+                .target(uri)
+                .queryParam(Claims.jti.name(), "a-123")
+                .queryParam(Claims.auth_time.name(), authTimeClaim);
+        Response response = echoEndpointTarget.request(MediaType.APPLICATION_JSON).header(HttpHeaders.AUTHORIZATION, "Bearer " + token).get();
+        Assert.assertEquals(response.getStatus(), HttpURLConnection.HTTP_OK);
+        JsonObject reply = response.readEntity(JsonObject.class);
+        System.out.println(reply);
+        Reporter.log(reply.toString());
+        Assert.assertTrue(reply.getBoolean("pass"), reply.getString("msg"));
+    }
+    @RunAsClient
+    @Test(groups = TEST_GROUP_CDI_JSON,
+            description = "Verify that the injected aud claim is as expected")
+    public void verifyInjectedAudience() throws Exception {
+        Reporter.log("Begin verifyInjectedAudience\n");
+        String uri = baseURL.toExternalForm() + "/endp/verifyInjectedAudience";
+        WebTarget echoEndpointTarget = ClientBuilder.newClient()
+                .target(uri)
+                .queryParam(Claims.aud.name(), "s6BhdRkqt3")
+                .queryParam(Claims.auth_time.name(), authTimeClaim);
+        Response response = echoEndpointTarget.request(MediaType.APPLICATION_JSON).header(HttpHeaders.AUTHORIZATION, "Bearer " + token).get();
+        Assert.assertEquals(response.getStatus(), HttpURLConnection.HTTP_OK);
+        JsonObject reply = response.readEntity(JsonObject.class);
+        System.out.println(reply);
+        Reporter.log(reply.toString());
+        Assert.assertTrue(reply.getBoolean("pass"), reply.getString("msg"));
+    }
+    @RunAsClient
+    @Test(groups = TEST_GROUP_CDI_JSON,
+            description = "Verify that the injected iat claim is as expected")
+    public void verifyInjectedIssuedAt() throws Exception {
+        Reporter.log("Begin verifyInjectedIssuedAt\n");
+        String uri = baseURL.toExternalForm() + "/endp/verifyInjectedIssuedAt";
+        WebTarget echoEndpointTarget = ClientBuilder.newClient()
+                .target(uri)
+                .queryParam(Claims.iat.name(), iatClaim)
+                .queryParam(Claims.auth_time.name(), authTimeClaim);
+        Response response = echoEndpointTarget.request(MediaType.APPLICATION_JSON).header(HttpHeaders.AUTHORIZATION, "Bearer " + token).get();
+        Assert.assertEquals(response.getStatus(), HttpURLConnection.HTTP_OK);
+        JsonObject reply = response.readEntity(JsonObject.class);
+        System.out.println(reply);
+        Reporter.log(reply.toString());
+        Assert.assertTrue(reply.getBoolean("pass"), reply.getString("msg"));
+    }
+    @RunAsClient
+    @Test(groups = TEST_GROUP_CDI_JSON,
+            description = "Verify that the injected raw token claim is as expected")
+    public void verifyInjectedAuthTime() throws Exception {
+        Reporter.log("Begin verifyInjectedAuthTime\n");
+        String uri = baseURL.toExternalForm() + "/endp/verifyInjectedAuthTime";
+        WebTarget echoEndpointTarget = ClientBuilder.newClient()
+                .target(uri)
+                .queryParam(Claims.auth_time.name(), authTimeClaim);
+        Response response = echoEndpointTarget.request(MediaType.APPLICATION_JSON).header(HttpHeaders.AUTHORIZATION, "Bearer " + token).get();
+        Assert.assertEquals(response.getStatus(), HttpURLConnection.HTTP_OK);
+        JsonObject reply = response.readEntity(JsonObject.class);
+        System.out.println(reply);
+        Reporter.log(reply.toString());
+        Assert.assertTrue(reply.getBoolean("pass"), reply.getString("msg"));
+    }
+    @RunAsClient
+    @Test(groups = TEST_GROUP_CDI_JSON,
+            description = "Verify that the injected customString claim is as expected")
+    public void verifyInjectedCustomString() throws Exception {
+        Reporter.log("Begin verifyInjectedCustomString\n");
+        String uri = baseURL.toExternalForm() + "/endp/verifyInjectedCustomString";
+        WebTarget echoEndpointTarget = ClientBuilder.newClient()
+                .target(uri)
+                .queryParam("value", "customStringValue")
+                .queryParam(Claims.auth_time.name(), authTimeClaim);
+        Response response = echoEndpointTarget.request(MediaType.APPLICATION_JSON).header(HttpHeaders.AUTHORIZATION, "Bearer " + token).get();
+        Assert.assertEquals(response.getStatus(), HttpURLConnection.HTTP_OK);
+        JsonObject reply = response.readEntity(JsonObject.class);
+        System.out.println(reply);
+        Reporter.log(reply.toString());
+        Assert.assertTrue(reply.getBoolean("pass"), reply.getString("msg"));
+    }
+    @RunAsClient
+    @Test(groups = TEST_GROUP_CDI_JSON,
+            description = "Verify that the injected customInteger claim is as expected")
+    public void verifyInjectedCustomInteger() throws Exception {
+        Reporter.log("Begin verifyInjectedCustomInteger\n");
+        String uri = baseURL.toExternalForm() + "/endp/verifyInjectedCustomInteger";
+        WebTarget echoEndpointTarget = ClientBuilder.newClient()
+                .target(uri)
+                .queryParam("value", 123456789)
+                .queryParam(Claims.auth_time.name(), authTimeClaim);
+        Response response = echoEndpointTarget.request(MediaType.APPLICATION_JSON).header(HttpHeaders.AUTHORIZATION, "Bearer " + token).get();
+        Assert.assertEquals(response.getStatus(), HttpURLConnection.HTTP_OK);
+        JsonObject reply = response.readEntity(JsonObject.class);
+        System.out.println(reply);
+        Reporter.log(reply.toString());
+        Assert.assertTrue(reply.getBoolean("pass"), reply.getString("msg"));
+    }
+    @RunAsClient
+    @Test(groups = TEST_GROUP_CDI_JSON,
+            description = "Verify that the injected customDouble claim is as expected")
+    public void verifyInjectedCustomDouble() throws Exception {
+        Reporter.log("Begin verifyInjectedCustomDouble\n");
+        String uri = baseURL.toExternalForm() + "/endp/verifyInjectedCustomDouble";
+        WebTarget echoEndpointTarget = ClientBuilder.newClient()
+                .target(uri)
+                .queryParam("value", 3.14159265358979323846)
+                .queryParam(Claims.auth_time.name(), authTimeClaim);
+        Response response = echoEndpointTarget.request(MediaType.APPLICATION_JSON).header(HttpHeaders.AUTHORIZATION, "Bearer " + token).get();
+        Assert.assertEquals(response.getStatus(), HttpURLConnection.HTTP_OK);
+        JsonObject reply = response.readEntity(JsonObject.class);
+        System.out.println(reply);
+        Reporter.log(reply.toString());
+        Assert.assertTrue(reply.getBoolean("pass"), reply.getString("msg"));
+    }
+
+}
---
+++ src/test/java/org/wildfly/swarm/mpjwtauth/container/jaxrs/JsonValuejectionEndpoint.java
@@ -0,0 +1,273 @@
+package org.wildfly.swarm.mpjwtauth.container.jaxrs;
+
+import java.util.List;
+
+import javax.annotation.security.DenyAll;
+import javax.inject.Inject;
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonNumber;
+import javax.json.JsonObject;
+import javax.json.JsonString;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import org.eclipse.microprofile.jwt.Claim;
+import org.eclipse.microprofile.jwt.Claims;
+
+@Path("/endp")
+@DenyAll
+public class JsonValuejectionEndpoint {
+    @Inject
+    @Claim("raw_token")
+    private JsonString rawToken;
+    @Inject
+    @Claim("iss")
+    private JsonString issuer;
+    @Inject
+    @Claim("jti")
+    private JsonString jti;
+    @Inject
+    @Claim("aud")
+    private JsonArray aud;
+    @Inject
+    @Claim("roles")
+    private JsonArray roles;
+    @Inject
+    @Claim("iat")
+    private JsonNumber issuedAt;
+    @Inject
+    @Claim("auth_time")
+    private JsonNumber authTime;
+    @Inject
+    @Claim("customString")
+    private JsonString customString;
+    @Inject
+    @Claim("customInteger")
+    private JsonNumber customInteger;
+    @Inject
+    @Claim("customDouble")
+    private JsonNumber customDouble;
+    @Inject
+    @Claim("customObject")
+    private JsonObject customObject;
+
+    @GET
+    @Path("/verifyInjectedIssuer")
+    @Produces(MediaType.APPLICATION_JSON)
+    public JsonObject verifyInjectedIssuer(@QueryParam("iss") String iss) {
+        boolean pass = false;
+        String msg;
+        String issValue = issuer.getString();
+        if(issValue == null || issValue.length() == 0) {
+            msg = Claims.iss.name()+"value is null or empty, FAIL";
+        }
+        else if(issValue.equals(iss)) {
+            msg = Claims.iss.name()+" PASS";
+            pass = true;
+        }
+        else {
+            msg = String.format("%s: %s != %s", Claims.iss.name(), issValue, iss);
+        }
+        JsonObject result = Json.createObjectBuilder()
+                .add("pass", pass)
+                .add("msg", msg)
+                .build();
+        return result;
+    }
+    @GET
+    @Path("/verifyInjectedRawToken")
+    @Produces(MediaType.APPLICATION_JSON)
+    public JsonObject verifyInjectedRawToken(@QueryParam("raw_token") String rt) {
+        boolean pass = false;
+        String msg;
+        // raw_token
+        String rawTokenValue = rawToken.getString();
+        if(rawTokenValue == null || rawTokenValue.length() == 0) {
+            msg = Claims.raw_token.name()+"value is null or empty, FAIL";
+        }
+        else if(rawTokenValue.equals(rt)) {
+            msg = Claims.raw_token.name()+" PASS";
+            pass = true;
+        }
+        else {
+            msg = String.format("%s: %s != %s", Claims.raw_token.name(), rawTokenValue, rt);
+        }
+        JsonObject result = Json.createObjectBuilder()
+                .add("pass", pass)
+                .add("msg", msg)
+                .build();
+        return result;
+    }
+    @GET
+    @Path("/verifyInjectedJTI")
+    @Produces(MediaType.APPLICATION_JSON)
+    public JsonObject verifyInjectedJTI(@QueryParam("jti") String jwtID) {
+        boolean pass = false;
+        String msg;
+        // jti
+        String jtiValue = jti.getString();
+        if(jtiValue == null || jtiValue.length() == 0) {
+            msg = Claims.jti.name()+"value is null or empty, FAIL";
+        }
+        else if(jtiValue.equals(jwtID)) {
+            msg = Claims.jti.name()+" PASS";
+            pass = true;
+        }
+        else {
+            msg = String.format("%s: %s != %s", Claims.jti.name(), jtiValue, jwtID);
+        }
+        JsonObject result = Json.createObjectBuilder()
+                .add("pass", pass)
+                .add("msg", msg)
+                .build();
+        return result;
+    }
+    @GET
+    @Path("/verifyInjectedAudience")
+    @Produces(MediaType.APPLICATION_JSON)
+    public JsonObject verifyInjectedAudience(@QueryParam("aud") String audience) {
+        boolean pass = false;
+        String msg;
+        // aud
+        List<JsonString> audValue = aud.getValuesAs(JsonString.class);
+        if(audValue == null || audValue.size() == 0) {
+            msg = Claims.aud.name()+"value is null or empty, FAIL";
+        }
+        else if(audValue.get(0).getString().equals(audience)) {
+            msg = Claims.aud.name()+" PASS";
+            pass = true;
+        }
+        else {
+            msg = String.format("%s: %s != %s", Claims.aud.name(), audValue, audience);
+        }
+        JsonObject result = Json.createObjectBuilder()
+                .add("pass", pass)
+                .add("msg", msg)
+                .build();
+        return result;
+    }
+    @GET
+    @Path("/verifyInjectedIssuedAt")
+    @Produces(MediaType.APPLICATION_JSON)
+    public JsonObject verifyInjectedIssuedAt(@QueryParam("iat") Long iat) {
+        boolean pass = false;
+        String msg;
+        // iat
+        Long iatValue = issuedAt.longValue();
+        if(iatValue == null || iatValue.intValue() == 0) {
+            msg = Claims.iat.name()+"value is null or empty, FAIL";
+        }
+        else if(iatValue.equals(iat)) {
+            msg = Claims.iat.name()+" PASS";
+            pass = true;
+        }
+        else {
+            msg = String.format("%s: %s != %s", Claims.iat.name(), iatValue, iat);
+        }
+        JsonObject result = Json.createObjectBuilder()
+                .add("pass", pass)
+                .add("msg", msg)
+                .build();
+        return result;
+    }
+    @GET
+    @Path("/verifyInjectedAuthTime")
+    @Produces(MediaType.APPLICATION_JSON)
+    public JsonObject verifyInjectedAuthTime(@QueryParam("auth_time") Long authTime) {
+        boolean pass = false;
+        String msg;
+        // auth_time
+        Long authTimeValue = this.authTime.longValue();
+        if(authTimeValue == null) {
+            msg = Claims.auth_time.name()+" value is null or missing, FAIL";
+        }
+        else if(authTimeValue.equals(authTime)) {
+            msg = Claims.auth_time.name()+" PASS";
+            pass = true;
+        }
+        else {
+            msg = String.format("%s: %s != %s", Claims.auth_time.name(), authTimeValue, authTime);
+        }
+        JsonObject result = Json.createObjectBuilder()
+                .add("pass", pass)
+                .add("msg", msg)
+                .build();
+        return result;
+    }
+
+    @GET
+    @Path("/verifyInjectedCustomString")
+    @Produces(MediaType.APPLICATION_JSON)
+    public JsonObject verifyInjectedCustomString(@QueryParam("value") String value) {
+        boolean pass = false;
+        String msg;
+        // iat
+        String customValue = customString.getString();
+        if(customValue == null || customValue.length() == 0) {
+            msg = "customString value is null or empty, FAIL";
+        }
+        else if(customValue.equals(value)) {
+            msg = "customString PASS";
+            pass = true;
+        }
+        else {
+            msg = String.format("customString: %s != %s", customValue, value);
+        }
+        JsonObject result = Json.createObjectBuilder()
+                .add("pass", pass)
+                .add("msg", msg)
+                .build();
+        return result;
+    }
+
+    @GET
+    @Path("/verifyInjectedCustomInteger")
+    @Produces(MediaType.APPLICATION_JSON)
+    public JsonObject verifyInjectedCustomInteger(@QueryParam("value") Long value) {
+        boolean pass = false;
+        String msg;
+        // iat
+        Long customValue = customInteger.longValue();
+        System.out.printf("+++ verifyInjectedCustomInteger, JsonNumber.class.CL: %s\n",
+                JsonNumber.class.getClassLoader());
+        if(customValue.equals(value)) {
+            msg = "customInteger PASS";
+            pass = true;
+        }
+        else {
+            msg = String.format("customInteger: %d != %d", customValue, value);
+        }
+        JsonObject result = Json.createObjectBuilder()
+                .add("pass", pass)
+                .add("msg", msg)
+                .build();
+        return result;
+    }
+
+    @GET
+    @Path("/verifyInjectedCustomDouble")
+    @Produces(MediaType.APPLICATION_JSON)
+    public JsonObject verifyInjectedCustomDouble(@QueryParam("value") Double value) {
+        boolean pass = false;
+        String msg;
+        // iat
+        Double customValue = customDouble.doubleValue();
+        if(Math.abs(customValue.doubleValue() - value.doubleValue()) < 0.00001) {
+            msg = "customDouble PASS";
+            pass = true;
+        }
+        else {
+            msg = String.format("customDouble: %s != %.8f", customValue, value);
+        }
+        JsonObject result = Json.createObjectBuilder()
+                .add("pass", pass)
+                .add("msg", msg)
+                .build();
+        return result;
+    }
+
+}
